2fd38ff49a6bbd10bb8d1311a46f296557b29834
Hugo <hugoxu11@gmail.com>
2022-03-10 12:40:05 -0500

    first commit

---
+++ random_advice_generator/random_advice_generator.js
@@ -0,0 +1,26 @@
+fetch("https://api.adviceslip.com/advice")
+    .then(myObject => myObject.text())
+    .then(myText => {
+        document.getElementById("advice-number").innerHTML = `ADVICE #${JSON.parse(myText).slip.id}`;
+        document.getElementById("quote").innerHTML = `"${JSON.parse(myText).slip.advice.replace(/â€˜/g, '\'').replace(/Ã¤/g, 'ä').replace(/dont/g, 'don\'t')}"`;
+    });
+
+function reload(file) {
+    fetch(file)
+        .then(myObject => myObject.text())
+        .then(myText => {
+            document.getElementById("advice-number").innerHTML = `ADVICE #${JSON.parse(myText).slip.id}`;
+            document.getElementById("quote").innerHTML = `"${JSON.parse(myText).slip.advice.replace(/â€˜/g, '\'').replace(/Ã¤/g, 'ä').replace(/dont/g, 'don\'t')}"`;  
+        });
+}
+
+window.addEventListener("keypress", function(event) {
+    if (event.code === "Space") {
+        fetch("https://api.adviceslip.com/advice")
+            .then(myObject => myObject.text())
+            .then(myText => {
+                document.getElementById("advice-number").innerHTML = `ADVICE #${JSON.parse(myText).slip.id}`;
+                document.getElementById("quote").innerHTML = `"${JSON.parse(myText).slip.advice.replace(/â€˜/g, '\'').replace(/Ã¤/g, 'ä').replace(/dont/g, 'don\'t')}"`;
+            });
+    }
+});
